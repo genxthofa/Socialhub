93596f8a92ee8d776f2afe058aa491ba5c94a2d0
Deepalakshmi <deepalakshmi.sasikumar@gmail.com>
2026-09-10 15:58:40 +0530

    Add Google site verification meta tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GenX Thofa - SocialHub</title>
+    <meta name="google-site-verification" content="e7J5pE5XznmCGl-TqrXLfqCytkXys5WKeaanU0qwdWU" />
     
     <!-- Include Vite Assets -->
     @viteReactRefresh
